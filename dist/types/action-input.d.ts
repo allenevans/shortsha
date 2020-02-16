e497b14a0a1f5bea36dcd7da3bd11a3f1ab08169
gitci <git-ci@techinity.com>
2020-02-16 16:52:02 +0000

    release artifacts

---
+++ dist/types/action-input.d.ts
@@ -0,0 +1,8 @@
+export interface ActionInput {
+    [key: string]: number | string;
+    length: number;
+    name: string;
+    offset: number;
+    sha: string;
+}
+//# sourceMappingURL=action-input.d.ts.map
---
+++ dist/utils/input-parse.d.ts
@@ -0,0 +1,3 @@
+import { ActionInput } from '../types/action-input';
+export declare const inputParse: (getInput: Function) => ActionInput;
+//# sourceMappingURL=input-parse.d.ts.map
